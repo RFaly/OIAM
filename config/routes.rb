8815404eec11e222aaffee1413d51fc1de9db8e2
RFaly <ramahafaly2.0@gmail.com>
2020-06-25 11:39:01 +0300

    controller for devise ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,40 @@ Rails.application.routes.draw do
   root to: 'static_page#home'
 	get '/wellcome', to: 'static_page#allHome', as: 'wellcome'
 
-
 	#~~~~~~~~~~~~~~~~~~~~ Client ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	get '/recruteur', to: 'client#main', as: 'main_recruiter'
-	devise_for :clients
+	devise_for :clients, path: 'clients'
 
 	#~~~~~~~~~~~~~~~~~~~~ Candidate ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	get '/cadre', to: 'cadre#main', as: 'main_cadre'
-	devise_for :cadres
+	devise_for :cadres, path: 'cadres'
 
 	#~~~~~~~~~~~~~~~~~~~~ Admin ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	devise_for :admins, path: 'admins'
 
 end
+
+
+
+# rails generate devise:controllers clients
+# rails generate devise:controllers admins
+
+
+=begin
+
+===============================================================================
+
+Some setup you must do manually if you haven't yet:
+
+  Ensure you have overridden routes for generated controllers in your routes.rb.
+  For example:
+
+    Rails.application.routes.draw do
+      devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+    end
+
+===============================================================================
+
+=end
