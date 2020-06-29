d950aaff720135130518ea31bc54e037cf6b6bc8
RFaly <ramahafaly2.0@gmail.com>
2020-06-29 09:13:55 +0300

    change

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -10,15 +10,15 @@ module Accessible
       flash.clear
       # if you have rails_admin. You can redirect anywhere really
       # redirect_to(rails_admin.dashboard_path) and return
-      redirect_to(authenticated_admin_root_path) and return
+      # redirect_to(authenticated_admin_root_path) and return
     elsif current_cadre
       flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
-      redirect_to(authenticated_cadre_root_path) and return
+      # redirect_to(authenticated_cadre_root_path) and return
     elsif current_client
     	flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
-      redirect_to(authenticated_client_root_path) and return
+      # redirect_to(devise_scope :client) and return
     end
   end
 end
