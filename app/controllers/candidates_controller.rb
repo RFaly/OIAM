39bd60c5cb5f6ebd1f3154f09ee76652bd93da57
RFaly <ramahafaly2.0@gmail.com>
2020-07-01 09:57:04 +0300

    save change

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,55 +1,46 @@
 class CandidatesController < ApplicationController
   before_action :authenticate_cadre!, except: :main
+  before_action :init_current_cadre, except: :main
 
   def main
   end
 
 # dashbord
   def my_profil # miaraka ny profil sy test
-    @cadre = current_cadre
-    if @cadre.potential_test.nil? || @cadre.skils_test.nil? || @cadre.fit_test.nil?
-      redirect_to my_tests_path
-    elsif @cadre.question1.nil? || @cadre.question2.nil? || @cadre.question3.nil? || @cadre.question4.nil? || @cadre.question5.nil?
-      redirect_to edit_profil_path
-    end
+    # if @cadre.potential_test.nil? || @cadre.skils_test.nil? || @cadre.fit_test.nil?
+    #   redirect_to my_tests_path
+    # elsif @cadre.question1.nil? || @cadre.question2.nil? || @cadre.question3.nil? || @cadre.question4.nil? || @cadre.question5.nil?
+    #   redirect_to edit_profil_path
+    # end
   end
 
 
   def my_tests
-    @cadre = current_cadre
   end
 
   def edit_profil
-    @cadre = current_cadre
   end
 
   def confirmedProfil
-    @cadre = current_cadre
     uploader = ImageUploader.new
-    uploader.store!(params[:cadre][:image])
-    
-    if uploader.url.nil?
-      redirect_back(fallback_location: root_path)
-      #error
-    else
+    if params[:cadre][:image].nil? && @cadre.image.nil?
+      redirect_to edit_profil_path, alert: "Image non trouvé"
+    elsif !params[:cadre][:image].nil?
+      uploader.store!(params[:cadre][:image])
       @cadre.image = uploader.url
-      @cadre.update(post_params)
       @cadre.save
-      redirect_to my_profil_path
     end
-
+    @cadre.update(post_params)
+    redirect_to my_profil_path
   end
 
 	def searchJob
-    @cadre = current_cadre
 	end
 
 	def favoriteJob
-    @cadre = current_cadre
 	end
 
 	def recrutmentMonitoring
-    @cadre = current_cadre
 	end
 
 # 3 test de recrutement
@@ -69,12 +60,13 @@ class CandidatesController < ApplicationController
   def post_params
     params.require(:cadre).permit(:question1,:question2,:question3,:question4,:question5)
   end
+
+  def init_current_cadre
+    @cadre = current_cadre
+  end
 end
 
 =begin
-  
-
-
 
 before_action :authenticate_cadre!
 cadre_signed_in?
@@ -88,8 +80,4 @@ before_action :authenticate_admin!
 admin_signed_in?
 current_admin
 
-
-
-
 =end
-
