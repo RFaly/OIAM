b8de208de4e67a5ed44db14c7e73c25b90f1df31
RFaly <ramahafaly2.0@gmail.com>
2020-07-09 14:57:14 +0300

    add errors

--- config/initializers/carrierwave.rb
+++
@@ -1,4 +0,0 @@
-CarrierWave.configure do |config|
-  carrierwave_gem = Bundler.rubygems.find_name('carrierwave').first
-  I18n.load_path.prepend(File.join(carrierwave_gem.full_gem_path, 'lib', 'carrierwave', 'locale', "en.yml")) if carrierwave_gem.present?
-end
