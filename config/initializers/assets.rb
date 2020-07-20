f3d60db73a96b4d9d9d518f40b1f169f134fb321
fastfabien <fastfabien56@gmail.com>
2020-07-20 10:47:37 +0300

    Page Fit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.precompile += %w( tmp_sign_up.css )
 Rails.application.config.assets.precompile += %w( my_test.css )
 Rails.application.config.assets.precompile += %w( potential_test.css )
 Rails.application.config.assets.precompile += %w( test_skills.css )
+Rails.application.config.assets.precompile += %w( test_fit.css )
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
