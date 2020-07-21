a6203d0a488809338fbfdf35a4d6960ba63347b6
fanomallot <aafano19@gmail.com>
2020-07-21 11:48:56 +0300

    inscription

---
+++ public/js/navbar.js
@@ -0,0 +1,33 @@
+$('.dropup1').focusOut(function () {
+  $('.navbar-content').removeClass('open');
+});
+$(document).ready(function () {
+  var path = window.location.pathname.split('/').pop();
+  console.log(`${path}`);
+
+  let target = $('nav a[href="/' + path + '"]');
+  target.addClass('act');
+
+  let check = true;
+  $('.btn-btn').click(function () {
+    if (check === true) {
+      $('.btn-btn-burger').addClass('open');
+      $('.navbar-content').fadeIn(800);
+      check = false;
+    } else {
+      $('.btn-btn-burger').removeClass('open');
+      $('.navbar-content').fadeOut(500);
+      check = true;
+    }
+  });
+  let check1 = true;
+  $(' .dropup-btn1').click(function () {
+    if (check1) {
+      $('.dropup-content1').fadeIn(800);
+      check1 = false;
+    } else {
+      $('.dropup-content1').fadeOut(500);
+      check1 = true;
+    }
+  });
+});
