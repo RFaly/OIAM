e75edf28b539cc907f638ed1caf6a2a912338c50
RFaly <ramahafaly2.0@gmail.com>
2020-07-21 07:49:20 +0300

    allHome change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	#~~~~~~~~~~~~~~~~~~~~ Accueil ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   root to: 'special_page#home'
-	get '/wellcome', to: 'static_page#allHome', as: 'wellcome'
+	get '/wellcome', to: 'special_page#allHome', as: 'wellcome'
   get '/méthodologie', to: 'static_page#methodology', as: 'methodology'
   get '/equipe', to: 'static_page#equipe', as: 'equipe'
   get '/portfolio', to: 'static_page#portfolio', as: 'portfolio'
