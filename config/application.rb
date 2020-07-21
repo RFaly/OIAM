c4f948571ba8365e3fe06cc4d94cde44b2415573
RFaly <ramahafaly2.0@gmail.com>
2020-07-21 20:51:43 +0300

    l

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,12 @@ module Oiam
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.initialize_on_precompile=false
 		config.i18n.available_locales = [:en, :fr]
 	  config.i18n.default_locale = :fr
   end
