8a62dc3ecc4041196b3a65bba0e9d71126ebb25e
fastfabien <fastfabien56@gmail.com>
2020-07-28 16:35:27 +0300

    Modifier compte candidats

--- public/js/edit_profil_cadre.js
+++ public/js/edit_profil_cadre.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+
+    $(".cv-champs").change(function() {
+   shwoNameCv(this);
+    });
+
+    function shwoNameCv(input) {
+     if (input.files && input.files[0]) {
+       var cv_filename_show = $('#cv-filename');
+       $(cv_filename_show).text(cv_filename);
+     }
+}
+ });
+
+
 var slider = document.getElementById("slide");
 var output = document.getElementById("value");
 
@@ -10,4 +25,7 @@ slider.oninput = function(){
     output.innerHTML = this.value;
     output.style.left = ((this.value-60)*97)/290 + "%";
 }
-    
+
+
+var filename = document.getElementById('cv-filename');
+var cv_filename = document.getElementsByClassName('cv-champs');
