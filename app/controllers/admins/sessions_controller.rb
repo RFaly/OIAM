d6702d29add72652ca4eca5339b7a497d1848350
RFaly <ramahafaly2.0@gmail.com>
2020-07-31 09:46:03 +0300

    correct buf

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -6,9 +6,10 @@ class Admins::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    @header = true
+    super
+  end
 
   # POST /resource/sign_in
   # def create
