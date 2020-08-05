c3c7fbbc44d752220a619bbed70e6ec72bb92cca
fanomallot <aafano19@gmail.com>
2020-08-05 14:37:18 +0300

    selection cadre

--- public/js/add_favorite_cadre.js
+++ public/js/add_favorite_cadre.js
@@ -1,41 +1,40 @@
-$(".add-js-toFavorite").click(function(){
-	let testDiv = $(".js-candidates-list[data-info=false]")
-	if (testDiv.length < 5) {
-		let number = $(this).data().info
-		let currentDiv = $(".js-candidates-list[data-info=true]")[0]
-		let image = $(".js-image-" + number).first().clone()
-		image.css('width',"100px")
-		image.css('height',"150px")
-		$(currentDiv).html(image);
-		currentDiv.dataset.info = false
-		currentDiv.dataset.element = number
-		$(".rmv-js-toFavorite[data-info="+number+"]").show()
-		$(this).hide()
-		$("#input_cadre_ids").val(actualiseValueInArray())
-	}
+$('.add-js-toFavorite').click(function () {
+  let testDiv = $('.js-candidates-list[data-info=false]');
+  if (testDiv.length < 5) {
+    let number = $(this).data().info;
+    let currentDiv = $('.js-candidates-list[data-info=true]')[0];
+    let image1 = $('.js-image-' + number);
+    var image = image1.first().clone();
+    $(currentDiv).html(image);
+    currentDiv.dataset.info = false;
+    currentDiv.dataset.element = number;
+    $('.rmv-js-toFavorite[data-info=' + number + ']').show();
+    $(this).hide();
+    $('#input_cadre_ids').val(actualiseValueInArray());
+  }
 });
 
-$(".rmv-js-toFavorite").click(function(){
-	let testDiv = $(".js-candidates-list[data-info=false]")
-	if (testDiv.length > 0) {
-		let number = $(this).data().info	
-		let currentDiv = $(".js-candidates-list[data-element="+number+"]")[0]
-		currentDiv.dataset.info = true
-		currentDiv.dataset.element = ""
-		$(currentDiv).html("");
-		$(".add-js-toFavorite[data-info="+number+"]").show()
-		$(this).hide()
-		$("#input_cadre_ids").val(actualiseValueInArray())
-	}
+$('.rmv-js-toFavorite').click(function () {
+  let testDiv = $('.js-candidates-list[data-info=false]');
+  if (testDiv.length > 0) {
+    let number = $(this).data().info;
+    let currentDiv = $('.js-candidates-list[data-element=' + number + ']')[0];
+    currentDiv.dataset.info = true;
+    currentDiv.dataset.element = '';
+    $(currentDiv).html('');
+    $('.add-js-toFavorite[data-info=' + number + ']').show();
+    $(this).hide();
+    $('#input_cadre_ids').val(actualiseValueInArray());
+  }
 });
 
-function actualiseValueInArray(){
-	let listArray = []
-	$(".js-candidates-list[data-info]").each(function(){
-		let element = this.dataset.element.toString()
-		if ( element.length >= 1 ) {
-			listArray.push(element)
-		}
-	});
-	return listArray
+function actualiseValueInArray() {
+  let listArray = [];
+  $('.js-candidates-list[data-info]').each(function () {
+    let element = this.dataset.element.toString();
+    if (element.length >= 1) {
+      listArray.push(element);
+    }
+  });
+  return listArray;
 }
