57166f4448a8fa570a0393e6583221d30d959525
andorama21 <ramanamihanta.ando@gmail.com>
2020-08-24 13:25:11 +0300

    admin routes

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -2,4 +2,12 @@ class AdminDashboardController < ApplicationAdminController
   before_action :authenticate_admin!
   def main
   end
+  def agenda
+  end
+  def offer
+  end
+  def candidate
+  end
+  def tache
+  end
 end
