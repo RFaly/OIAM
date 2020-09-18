43f8514fccacaa3bcde467e75a01ea1f4c472c2b
RFaly <ramahafaly2.0@gmail.com>
2020-09-18 14:24:23 +0300

    remove column avis_recruteur

--- db/migrate/20200918094738_add_compte_rendu_at_cadre_infos.rb
+++ db/migrate/20200918094738_add_compte_rendu_at_cadre_infos.rb
@@ -1,5 +1,6 @@
 class AddCompteRenduAtCadreInfos < ActiveRecord::Migration[5.2]
   def change
+  	remove_column :cadre_infos, :avis_recruteur, :text
   	add_column :cadre_infos, :compte_rendu, :string
   	add_column :cadre_infos, :avis, :text
   end
