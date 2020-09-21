3cde9eca313de7a3006fb9fb46801c1e75a02e0a
fastfabien <fastfabien56@gmail.com>
2020-09-21 09:28:55 +0300

    Felicitatin cadre

---
+++ public/js/congratulation_cadre.js
@@ -0,0 +1,29 @@
+function addDiamond(){
+	var modal = document.getElementById("modal-unique")
+	var nb = Math.random() * 40;
+	const diamond = document.createElement('div');
+	const image = document.createElement('img');
+
+
+	image.src = '/image/Vector-24.png';
+	diamond.style.left = Math.random() * 100 +"vw";
+	diamond.style.animationDuration = Math.random() * 2 + 3 + "s";
+	diamond.classList.add("diamond");
+	diamond.style.width = Math.random(5) * nb + "px";
+	diamond.style.height = Math.random(5) * nb + "px";
+
+	modal.appendChild(diamond);
+	diamond.appendChild(image);
+
+	setTimeout(() => {
+		diamond.remove();
+	}, 5000);
+}
+
+var interval = Math.random() * 300;
+
+setInterval(addDiamond, interval);
+
+
+
+
