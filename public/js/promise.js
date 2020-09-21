ab1c8c8d440f33211fbd0812239db73e6e710103
fanomallot <aafano19@gmail.com>
2020-09-21 10:12:11 +0300

    modification site ++

--- public/js/promise.js
+++ public/js/promise.js
@@ -1,57 +1,76 @@
-var pop = 12;
-$('#renumeration-fixe').on('input', function () {
-  if ($('#post_oui').is(':checked')) {
-    var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
-    $('#pc-calcul').html(oiam.toFixed(2) + '€');
-  } else {
-    var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
-    $('#pc-calcul').html(oiam.toFixed(2) + '€');
-  }
-});
-$('#post_12').on('click', function () {
-  pop = 12;
-  if ($('#post_oui').is(':checked')) {
-    var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
-    $('#pc-calcul').html(oiam.toFixed(2) + '€');
-  } else {
-    var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
-    $('#pc-calcul').html(oiam.toFixed(2) + '€');
-  }
-});
-$('#post_13').on('click', function () {
-  pop = 13;
-  if ($('#post_oui').is(':checked')) {
-    var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
-    $('#pc-calcul').html(oiam.toFixed(2) + '€');
-  } else {
-    var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
-    $('#pc-calcul').html(oiam.toFixed(2) + '€');
-  }
-});
-$('#input-perso').on('input', function () {
-  pop = $(this).val();
-  if ($('#post_oui').is(':checked')) {
-    var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
-    $('#pc-calcul').html(oiam.toFixed(2) + '€');
-  } else {
-    var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
-    $('#pc-calcul').html(oiam.toFixed(2) + '€');
-  }
-});
+$(document).ready(function () {
+  let pop = 12;
+  $('#renumeration-fixe').on('input', function () {
+    if ($('#post_oui').is(':checked')) {
+      var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    } else {
+      var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    }
+  });
+  $('#post_12').on('click', function () {
+    pop = 12;
+    if ($('#post_oui').is(':checked')) {
+      var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    } else {
+      var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    }
+  });
+  $('#post_13').on('click', function () {
+    pop = 13;
+    if ($('#post_oui').is(':checked')) {
+      var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    } else {
+      var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    }
+  });
+  $('#post_perso').on('click', function () {
+    pop = $('#input-perso').val();
+    if ($('#post_oui').is(':checked')) {
+      var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    } else {
+      var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    }
+  });
 
-$('#js-input-choix').on('input', function () {
-  var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
-  $('#pc-calcul').html(oiam.toFixed(2) + '€');
-});
-$('#post_nom').on('focus', function () {
-  var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
-  $('#pc-calcul').html(oiam.toFixed(2) + '€');
-});
+  $('#input-perso').on('input', function () {
+    pop = $(this).val();
+    if ($('#post_oui').is(':checked')) {
+      var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    } else {
+      var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
+      $('#pc-calcul').html(oiam.toFixed(2) + '€');
+    }
+  });
 
-if ($('#post_oui').is(':checked')) {
-  var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
-  $('#pc-calcul').html(oiam.toFixed(2) + '€');
-} else {
-  var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
-  $('#pc-calcul').html(oiam.toFixed(2) + '€');
-}
+  $('#js-input-choix').on('input', function () {
+    var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
+    $('#pc-calcul').html(oiam.toFixed(2) + '€');
+  });
+  $('#post_nom').on('focus', function () {
+    var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
+    $('#pc-calcul').html(oiam.toFixed(2) + '€');
+  });
+  //  on load
+  if ($('#post_13').is(':checked')) {
+    pop = 13;
+  }
+  if ($('#post_perso').is(':checked')) {
+    pop = $('#input-perso').val();
+  }
+  if ($('#post_oui').is(':checked')) {
+    var oiam = (($('#renumeration-fixe').val() * 1000 * pop + $('#js-input-choix').val() * 1000) * 15) / 100;
+    $('#pc-calcul').html(oiam.toFixed(2) + '€');
+  } else {
+    var oiam = ($('#renumeration-fixe').val() * 1000 * pop * 15) / 100;
+    $('#pc-calcul').html(oiam.toFixed(2) + '€');
+  }
+});
