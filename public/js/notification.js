4a8243e8c1f521f4fad50f66d633711c3cad955f
fanomallot <aafano19@gmail.com>
2020-09-25 16:02:02 +0300

    modification notification

---
+++ public/js/notification.js
@@ -0,0 +1,28 @@
+if (document.URL.indexOf('#') == -1) {
+  url = document.URL + '#';
+  location = '#';
+  location.reload(true);
+}
+var month = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+var day = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+let $elementDate = $('.currentDateEntretien');
+
+$elementDate.each(function () {
+  let dateUTC = moment.utc($(this).data().times);
+  let dateGet = new Date(dateUTC.local());
+  var minut = dateGet.getMinutes();
+  var hour = dateGet.getHours();
+  var daily = dateGet.getDate();
+  var today = day[dateGet.getDay()];
+  if (daily < 10) {
+    daily = '0' + daily;
+  }
+  if (minut < 10) {
+    minut = '0' + minut;
+  }
+  if (hour < 10) {
+    hour = '0' + hour;
+  }
+  let dateString = today + ' ' + daily + ' ' + month[dateGet.getMonth()] + ' ' + dateGet.getFullYear();
+  $(this).html(dateString + ' à ' + hour + 'h:' + minut);
+});
