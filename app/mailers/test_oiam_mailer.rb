1d0a18bcbb38d1c377b0f4d984cea0e2ee4452da
RFaly <ramahafaly2.0@gmail.com>
2020-09-29 19:22:26 +0300

    sabve hcanghùsdfjsdf

--- app/mailers/test_oiam_mailer.rb
+++ app/mailers/test_oiam_mailer.rb
@@ -7,7 +7,7 @@ class TestOiamMailer < ApplicationMailer
   #
   def test_fit_accepted(cadre_info)
     @cadre_info = cadre_info
-    mail(to: [@cadre_info.mail], subject: 'Félicitations et Bienvenue !') 
+    mail(to: @cadre_info.mail, subject: 'Félicitations et Bienvenue !') 
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,7 +17,7 @@ class TestOiamMailer < ApplicationMailer
   #
   def test_fit_refused(cadre_info)
     @cadre_info = cadre_info
-    mail(to: [@cadre_info.mail], subject: 'Email de validation de compte visiteur') 
+    mail(to: @cadre_info.mail, subject: 'Email de validation de compte visiteur') 
   end
   #TestOiamMailer.test_fit_accepted.deliver_now
 end
