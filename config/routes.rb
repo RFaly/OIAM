048ed4bd1feb5eb35d1876edd2d3fd37219240aa
fastfabien <fastfabien56@gmail.com>
2020-10-08 15:06:53 +0300

    Condition generale

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/equipe', to: 'static_page#equipe', as: 'equipe'
   get '/portfolio', to: 'static_page#portfolio', as: 'portfolio'
   get '/contact', to: 'static_page#contact', as: 'contact'
+  get '/condition_general', to: 'static_page#condition_general', as: 'condition_general'
 
   get '/oiam', to: 'static_page#nothing', as: 'nothing'
 
@@ -198,7 +199,7 @@ Rails.application.routes.draw do
   #route message admin
   get 'secret-oiam-page/admin/messages/:id', to: 'admin_main#show_messaging', as: 'admin_show_messaging'
   post 'secret-oiam-page/admin/send-message', to: 'admin_main#post_messaging', as: 'post_messaging'
-  get 'secret-oiam-page/admin/messages/:id/recruteur', to: 'admin_main#show_message_recruteur', as:'show_message_recruteur' 
+  get 'secret-oiam-page/admin/messages/:id/recruteur', to: 'admin_main#show_message_recruteur', as:'show_message_recruteur'
   post 'secret-oiam-page/admin/send-message-recruteur', to: 'admin_main#post_message_recruteur', as: 'post_message_recruteur'
 
   # route admin cadre
