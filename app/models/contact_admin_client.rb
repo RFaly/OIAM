97ff4ea891b1ba3f0cbf905c02b3caa10c8cce12
Lova15 <ericlovatiana1506@gmail.com>
2020-10-13 14:04:09 +0300

    maj message

--- app/models/contact_admin_client.rb
+++ app/models/contact_admin_client.rb
@@ -3,4 +3,8 @@ class ContactAdminClient < ApplicationRecord
 	belongs_to :client
 	has_many :message_admin_clients
 
+	def mp_admin_not_see
+		return self.message_admin_clients.where(admin_see:false).count
+	end
+
 end
