c4cc565e60da02dc91ac47eb41e1fe1baf0fe6bb
RFaly <ramahafaly2.0@gmail.com>
2020-10-13 13:05:11 +0300

    ok ok

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -23,6 +23,7 @@ class StaticPageController < ApplicationController
     @header = true
   end
 
+  # save contacter nous
   def save_contact_us
     contact = ContactU.new(first_name: params[:first_name],last_name: params[:last_name],society: params[:society],function: params[:function],email: params[:mail],telephone: params[:telephone],how: params[:how],message: params[:message])
     if contact.save
@@ -33,6 +34,7 @@ class StaticPageController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  # save mail for contact
   def save_app_subscriber
     subscriber = Subscriber.new(email:params[:email])
     unless Subscriber.find_by(email:params[:email]).nil?
