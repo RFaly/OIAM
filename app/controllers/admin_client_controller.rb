001cd6aa6e2df4c7dd41fa632ca039769807c802
RFaly <ramahafaly2.0@gmail.com>
2020-10-14 13:29:58 +0300

    suivi recrutment ok admin

--- app/controllers/admin_client_controller.rb
+++ app/controllers/admin_client_controller.rb
@@ -3,10 +3,14 @@ class AdminClientController < ApplicationAdminController
 
   #recrutement en cours
   def main
-    @offre = OffreJob.all
-    @cadre = Cadre.all
+    @offres = OffreJob.all
   end
-  
+
+  #show recrutement en cours
+  def show_recrutment
+    @offre = OffreJob.find_by(id: params[:id])
+  end
+
   def offer
   	@offre = OffreJob.all
   end
