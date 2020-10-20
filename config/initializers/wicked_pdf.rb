0c305643c03df3db00eaff612b8bcf8587945f21
fanomallot <aafano19@gmail.com>
2020-10-20 15:54:26 +0300

    quelque modification

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -10,7 +10,7 @@
 WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
-   exe_path: '/usr/local/bin/wkhtmltopdf'
+  #  exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
   # exe_path: "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary'].version}/bin/wkhtmltopdf"
