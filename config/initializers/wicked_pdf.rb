b26db490ef4f2e523d98eec9955b700620dc51b6
fanomallot <aafano19@gmail.com>
2020-10-20 14:53:20 +0300

    quelque modification

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,12 +8,13 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.config = {
+# WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  # exe_path: "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary'].version}/bin/wkhtmltopdf"
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
@@ -24,4 +25,10 @@ WickedPdf.config = {
   # 'xvfb-run' command, in order to simulate an X server.
   #
   # use_xvfb: true,
-}
+# }
+if Rails.env.staging? || Rails.env.production?
+  exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+else
+  exe_path = Rails.root.join('bin', 'wkhtmltopdf').to_s
+  # exe_path = '/usr/local/bin/wkhtmltopdf'
+end
