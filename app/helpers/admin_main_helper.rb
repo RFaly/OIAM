031743036825837b9f3f708105b1aaabb0273c66
RFaly <ramahafaly2.0@gmail.com>
2020-10-21 09:01:42 +0300

    delete all page useless

--- app/helpers/admin_main_helper.rb
+++
@@ -1,2 +0,0 @@
-module AdminMainHelper
-end
--- test/controllers/admin_dashboard_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AdminDashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get main" do
-    get admin_dashboard_main_url
-    assert_response :success
-  end
-
-end
