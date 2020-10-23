7a9cf0da1aa458e9d7877403ec3d6189d7c72bcf
fastfabien <fastfabien56@gmail.com>
2020-10-23 17:31:00 +0300

    responsive left link

---
+++ public/js/menu_cadre.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  // js btn burger menu recruteur
+  let check = true;
+  $('.btn-menu-r').click(function () {
+    if (check === true) {
+      $('.btn-menu-burger').removeClass('open');
+      $('.left-menu-r').addClass('open');
+      $('.left-menu-photo').addClass('open');
+      $('.left-menu-content').fadeOut(500);
+      if ($(window).width() < 1200) {
+        $('.line-white2').css('height', $('.code-naf-flex').height() + 5);
+      } else {
+        $('.line-white2').css('height', $('.code-naf-flex').height() + 25);
+
+      }
+      check = false;
+    } else {
+      $('.btn-menu-burger').addClass('open');
+      $('.left-menu-r').removeClass('open');
+      $('.left-menu-photo').removeClass('open');
+      $('.left-menu-content').fadeIn(800);
+      if ($(window).width() < 1200) {
+        $('.line-white2').css('height', $('.code-naf-flex').height() + 45);
+      } else {
+        $('.line-white2').css('height', $('.code-naf-flex').height() + 25);
+
+      }
+      check = true;
+    }
+  });
+});
