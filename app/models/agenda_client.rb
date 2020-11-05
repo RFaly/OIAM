37d42c833ce2884940f47f5b3ac3fddf90d1c856
fanomallot <aafano19@gmail.com>
2020-11-05 08:59:09 +0300

    help admin

--- app/models/agenda_client.rb
+++ app/models/agenda_client.rb
@@ -1,20 +1,20 @@
 class AgendaClient < ApplicationRecord
 	belongs_to :offre_for_candidate
 
-	after_create :notifed_admin_if_first_candidate
+	# after_create :notifed_admin_if_first_candidate
 
 	# 5. Recherche et sélectionne des candidats
-	def notifed_admin_if_first_candidate
-		if self.offre_for_candidate.offre_job.offre_for_candidates.where(accepted_postule:true).count == 1
-			ProcessedHistory.create(
-        image: "/image/work.png",
-        message: "SELECTION CANDIDATS",
-        link: "<a href='#{clients_bp_offre_job_no_cadre_path(self.offre_for_candidate.offre_job.id)}'>VOIR</a>",
-        is_client:true,
-        genre: 1
-      )
-		end
-	end
+	# def notifed_admin_if_first_candidate
+	# 	if self.offre_for_candidate.offre_job.offre_for_candidates.where(accepted_postule:true).count == 1
+	# 		ProcessedHistory.create(
+    #     image: "/image/work.png",
+    #     message: "SELECTION CANDIDATS",
+    #     link: "<a href='#{clients_bp_offre_job_no_cadre_path(self.offre_for_candidate.offre_job.id)}'>VOIR</a>",
+    #     is_client:true,
+    #     genre: 1
+    #   )
+	# 	end
+	# end
 	
 =begin
 
