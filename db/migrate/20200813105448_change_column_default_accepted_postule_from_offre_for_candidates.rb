0280f092c4f607b13786becd1dc4b3398de88a0e
RFaly <ramahafaly2.0@gmail.com>
2020-11-06 13:59:36 +0300

    edit migration

--- db/migrate/20200813105448_change_column_default_accepted_postule_from_offre_for_candidates.rb
+++ db/migrate/20200813105448_change_column_default_accepted_postule_from_offre_for_candidates.rb
@@ -1,5 +1,9 @@
 class ChangeColumnDefaultAcceptedPostuleFromOffreForCandidates < ActiveRecord::Migration[5.2]
-  def change
+  def up
   	change_column_default(:offre_for_candidates, :accepted_postule, nil)
-  end
+	end
+
+	def down
+	  change_column_default(:offre_for_candidates, :accepted_postule, false)
+	end
 end
