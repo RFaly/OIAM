cb98da881de1690264d703967d1420aaf5ed0d6b
RFaly <ramahafaly2.0@gmail.com>
2020-11-11 10:49:42 +0300

    ok ok

--- db/migrate/20201110164346_add_cadre_and_client_to_processed_history.rb
+++ db/migrate/20201110164346_add_cadre_and_client_to_processed_history.rb
@@ -2,5 +2,6 @@ class AddCadreAndClientToProcessedHistory < ActiveRecord::Migration[5.2]
   def change
 		add_reference :processed_histories, :client
 		add_reference :processed_histories, :cadre_info
+		add_column :processed_histories, :see, :boolean, default: :false
   end
 end
