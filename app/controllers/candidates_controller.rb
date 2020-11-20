da57e97ebaf97684f143a5e6cc7a36163f30eba3
fanomallot <aafano19@gmail.com>
2020-11-20 15:04:21 +0300

    modification

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -118,7 +118,7 @@ class CandidatesController < ApplicationController
 
 	def searchJob
     validate_info_cadre
-    @offre_par_page = 1
+    @offre_par_page = 20
     @metiers = Metier.all
     @regions = Region.all
 
